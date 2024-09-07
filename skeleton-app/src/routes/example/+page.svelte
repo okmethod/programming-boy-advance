d8439a9643c2f65cee99d2083231130139be1b2d
shohei.okugi <shohei.okugi@ghelia.com>
2024-09-07 21:17:49 +0900

    :+1: safeEval

--- skeleton-app/src/routes/example/+page.svelte
+++ skeleton-app/src/routes/example/+page.svelte
@@ -6,18 +6,54 @@ function helloWorld() {
   console.log('Hello, world!');
 }
 
-helloWorld();
+//helloWorld();
+customFunction();
 
 `;
 
   let code = sampleCode;
   function executeCode(): void {
     try {
-      eval(code);
+      safeEval(code);
       console.log("Executed successfully.");
     } catch (error) {
       console.error("Failed to execute code:", error);
     }
+  }
+
+  function customFunction() {
+    console.log("Custom function called.");
+  }
+
+  function safeEval(code: string): unknown {
+    const allowedGlobals: Record<string, unknown> = {
+      customFunction: customFunction,
+      // 必要に応じて追加
+    };
+
+    const proxy = new Proxy(allowedGlobals, {
+      has: () => true,
+      get: (target, prop) => {
+        if (typeof prop === "string" && prop in target) {
+          return target[prop];
+        }
+        return undefined;
+      },
+    });
+
+    // 新しい関数を作成し、プロキシを使用してコードを実行
+    const executeCode = (code: string, context: any) => {
+      return new Function(
+        "context",
+        `
+          with (context) {
+            ${code}
+          }
+        `,
+      )(context);
+    };
+
+    return executeCode(code, proxy);
   }
 </script>
 
