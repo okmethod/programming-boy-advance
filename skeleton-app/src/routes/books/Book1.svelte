ff7a38ed624613d6f9b51dd7114d87207aea1ea2
shohei.okugi <shohei.okugi@ghelia.com>
2024-09-08 22:04:26 +0900

    :sparkles: write book1 let's execute program

--- skeleton-app/src/routes/books/Book1.svelte
+++ skeleton-app/src/routes/books/Book1.svelte
@@ -17,11 +17,39 @@ return str;`;
   const allowedGlobals: AllowedGlobals = {};
 </script>
 
-<div>
-  <h2 class="cBookTitle">Book1 : 結果の出力</h2>
+<div class="max-w-[1000px]">
+  <h2 class="cBookTitle">Book1 : プログラムを実行してみよう！</h2>
+  <hr class="cBorderLine" />
+  <div class="font-pixel12">
+    <p>
+      画面左側の <span class="cIndexSpan">Code Editor</span> に書かれたプログラムを、 画面中央の
+      <span class="px-2 py-1 font-pixel12 text-white bg-blue-500 rounded">Execute</span> ボタンを押して実行してみましょう。
+    </p>
+    <p>
+      プログラムを実行すると、
+      <span class="cBookCodeBlock hljs-keyword">return</span> が結果を出力します。 結果は、画面右側の
+      <span class="cIndexSpan">Result</span> に表示されます。
+    </p>
+    <p>
+      また、実行中に
+      <span class="cBookCodeBlock hljs-title">log()</span> がログを出力します。 ログは、画面右側の
+      <span class="cIndexSpan">logs</span> に表示されます。
+    </p>
+  </div>
   <hr class="cBorderLine" />
   <div class="cResponsiveCodeExecuter">
     <CodeExecuter bind:this={codeExecuterRef} bind:codeExeProps {allowedGlobals} />
+  </div>
+  <hr class="cBorderLine" />
+  <div class="font-pixel12">
+    <p>
+      もちろん、プログラムは書き換えることができます。<br />
+      例えば、このプログラムでは
+      <span class="cBookCodeBlock"><span class="hljs-keyword">const</span> <span class="text-white">str</span></span>
+      という変数に
+      <span class="cBookCodeBlock hljs-string">"Hello, World!"</span> という文字列を代入しています。<br />
+      もし、この文字列を書き換えてから実行したら、結果はどうなるでしょうか？
+    </p>
   </div>
   <hr class="cBorderLine" />
 </div>
