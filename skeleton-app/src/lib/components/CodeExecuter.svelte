7145570961c461c3d300ccdfd32ba9c36a25d89f
shohei.okugi <shohei.okugi@ghelia.com>
2024-09-08 10:03:32 +0900

    :recycle: put out components/CodeExecuter

---
+++ skeleton-app/src/lib/components/CodeExecuter.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+  import { getToastStore } from "@skeletonlabs/skeleton";
+  import HighlightCodeEditor from "$lib/components/HighlightCodeEditor.svelte";
+  import { executeEval, type AllowedGlobals } from "$lib/utils/safeEval";
+  import { simpleToast } from "$lib/utils/toastSettings";
+
+  export let code: string;
+  export let resultString: string;
+  export let logs: string[];
+  export let allowedGlobals: AllowedGlobals;
+
+  const allowedGlobalsDefault: AllowedGlobals = {
+    log: log,
+    Math: Math,
+    Date: Date,
+    // console: console,
+    // 必要に応じて追加
+  };
+
+  const toastStore = getToastStore();
+
+  function log(message: string): void {
+    const timestamp = new Date().toLocaleString();
+    logs.push(`[${timestamp}] ${message}`);
+    logs = [...logs];
+  }
+
+  function handleExecute(): void {
+    const result = executeEval(code, { ...allowedGlobalsDefault, ...allowedGlobals });
+    resultString = result.resultString;
+    toastStore.trigger(simpleToast(result.message, result.succeed));
+  }
+
+  function clearCode(): void {
+    code = "";
+  }
+  function clearResult(): void {
+    resultString = "";
+  }
+  function clearLogs(): void {
+    logs = [];
+  }
+</script>
+
+<div class="col-span-1 lg:col-span-5">
+  <div class="flex justify-between ml-2">
+    <strong class="cIndexSpan">Code Editor</strong>
+    <button type="submit" on:click={clearCode} class="relative z-30"> ✕ </button>
+  </div>
+  <HighlightCodeEditor bind:code cLanguage="language-javascript" />
+</div>
+
+<div class="col-span-1 lg:col-span-2 flex justify-center items-center">
+  <button type="submit" on:click={handleExecute} class="cIconButtonStyle relative">
+    <div class="cButtonSpan">
+      <span> Execute </span>
+    </div>
+  </button>
+</div>
+
+<div class="col-span-1 lg:col-span-5 space-y-4">
+  <div>
+    <div class="flex justify-between ml-2">
+      <strong class="cIndexSpan">Result</strong>
+      <button type="submit" on:click={clearResult} class="relative z-30"> ✕ </button>
+    </div>
+    <div class="w-96 h-24 p-4 border border-gray-300 rounded-md overflow-y-auto">
+      <span class="block font-pixel12">{resultString ?? ""}</span>
+    </div>
+  </div>
+  <div>
+    <div class="flex justify-between ml-2">
+      <strong class="cIndexSpan">Logs</strong>
+      <button type="submit" on:click={clearLogs} class="relative z-30"> ✕ </button>
+    </div>
+    <div class="w-96 h-40 p-4 border border-gray-300 rounded-md space-y-1 overflow-y-auto">
+      {#each logs as log}
+        <span class="block border border-gray-100 rounded-sm font-pixel10">{log}</span>
+      {/each}
+    </div>
+  </div>
+</div>
--- skeleton-app/src/routes/executer/+page.svelte
+++ skeleton-app/src/routes/executer/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-  import { getToastStore } from "@skeletonlabs/skeleton";
-  import HighlightCodeEditor from "$lib/components/HighlightCodeEditor.svelte";
-  import { executeEval, type AllowedGlobals } from "$lib/utils/safeEval";
-  import { simpleToast } from "$lib/utils/toastSettings";
-
-  const toastStore = getToastStore();
+  import type { AllowedGlobals } from "$lib/utils/safeEval";
+  import CodeExecuter from "$lib/components/CodeExecuter.svelte";
 
   const sampleCode = `
 const oddNumbers = [];
@@ -20,42 +16,17 @@ return oddNumbers;
 `;
 
   let logs: string[] = [];
-  function log(message: string): void {
-    const timestamp = new Date().toLocaleString();
-    logs.push(`[${timestamp}] ${message}`);
-    logs = [...logs];
-  }
+  let code = sampleCode;
+  let resultString: string;
 
   function customFunction(): void {
-    log("Custom function called.");
+    console.log("Custom function called.");
   }
 
   const allowedGlobals: AllowedGlobals = {
-    log: log,
     customFunction: customFunction,
-    Math: Math,
-    Date: Date,
-    // console: console,
     // 必要に応じて追加
   };
-
-  let code = sampleCode;
-  let resultString: string;
-  function handleExecute(): void {
-    const result = executeEval(code, allowedGlobals);
-    resultString = result.resultString;
-    toastStore.trigger(simpleToast(result.message, result.succeed));
-  }
-
-  function clearCode(): void {
-    code = "";
-  }
-  function clearResult(): void {
-    resultString = "";
-  }
-  function clearLogs(): void {
-    logs = [];
-  }
 </script>
 
 <div class="cRouteBodyStyle">
@@ -66,43 +37,6 @@ return oddNumbers;
 
   <!-- コンテンツ部 -->
   <div class="grid grid-cols-1 lg:grid-cols-12 justify-center items-center gap-4 m-4">
-    <div class="col-span-1 lg:col-span-5">
-      <div class="flex justify-between ml-2">
-        <strong class="cIndexSpan">Code Editor</strong>
-        <button type="submit" on:click={clearCode} class="relative z-30"> ✕ </button>
-      </div>
-      <HighlightCodeEditor bind:code cLanguage="language-javascript" />
-    </div>
-
-    <div class="col-span-1 lg:col-span-2 flex justify-center items-center">
-      <button type="submit" on:click={handleExecute} class="cIconButtonStyle relative">
-        <div class="cButtonSpan">
-          <span> Execute </span>
-        </div>
-      </button>
-    </div>
-
-    <div class="col-span-1 lg:col-span-5 space-y-4">
-      <div>
-        <div class="flex justify-between ml-2">
-          <strong class="cIndexSpan">Result</strong>
-          <button type="submit" on:click={clearResult} class="relative z-30"> ✕ </button>
-        </div>
-        <div class="w-96 h-24 p-4 border border-gray-300 rounded-md overflow-y-auto">
-          <span class="block font-pixel12">{resultString ?? ""}</span>
-        </div>
-      </div>
-      <div>
-        <div class="flex justify-between ml-2">
-          <strong class="cIndexSpan">Logs</strong>
-          <button type="submit" on:click={clearLogs} class="relative z-30"> ✕ </button>
-        </div>
-        <div class="w-96 h-40 p-4 border border-gray-300 rounded-md space-y-1 overflow-y-auto">
-          {#each logs as log}
-            <span class="block border border-gray-100 rounded-sm font-pixel10">{log}</span>
-          {/each}
-        </div>
-      </div>
-    </div>
+    <CodeExecuter bind:code bind:resultString bind:logs {allowedGlobals} />
   </div>
 </div>
