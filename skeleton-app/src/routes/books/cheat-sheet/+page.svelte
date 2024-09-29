edc4afa006c22e76c69b1397bbf68e6d4a9d1678
shohei.okugi <shohei.okugi@ghelia.com>
2024-09-29 16:57:07 +0900

    :+1: add javascript cheet sheet

--- skeleton-app/src/routes/books/cheat-sheet/+page.svelte
+++ skeleton-app/src/routes/books/cheat-sheet/+page.svelte
@@ -1,4 +1,64 @@
 <script lang="ts">
+  import HighlightCodeEditor from "$lib/components/HighlightCodeEditor.svelte";
+
+  const codeSnippets = [
+    {
+      title: "変数",
+      code: `
+// 再代入可能な変数
+let x = 10;
+
+// 再代入不可な定数
+const y = 20;
+
+// 配列
+const list = [1, 2, 3, 4, 5];
+`,
+    },
+    {
+      title: "関数",
+      code: `
+function greet(name) {
+  return \`Hello, \${name}!\`;
+}`,
+    },
+    {
+      title: "if分岐",
+      code: `
+if (x > 10) {
+  console.log("x は 10 より大きい");
+} else if (x === 10) {
+  console.log("x は 10 と等しい");
+} else {
+  console.log("x は 10 より小さい");
+}`,
+    },
+    {
+      title: "forループ",
+      code: `
+// 5回繰り返す
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// 配列の要素数の数だけ繰り返す
+const list = [1, 2, 3, 4, 5];
+for (const item of list) {
+  console.log(item);
+}
+`,
+    },
+  ];
 </script>
 
-<div class="cRouteBodyStyle">cheat sheet</div>
+<div class="cBookContents">
+  <h2 class="cLessonTitle">チートシート</h2>
+  <hr class="cBorderLine" />
+
+  <div class="p-4 rounded-lg">
+    {#each codeSnippets as snippet}
+      <h3 class="text-lg font-bold mt-4">{snippet.title}</h3>
+      <HighlightCodeEditor code={snippet.code} cLanguage="language-javascript" />
+    {/each}
+  </div>
+</div>
