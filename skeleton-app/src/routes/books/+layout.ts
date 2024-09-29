82b70078a755765f52798d8166eac3d32e3804fb
shohei.okugi <shohei.okugi@ghelia.com>
2024-09-29 16:00:59 +0900

    :+1: use path cheat-sheet

--- skeleton-app/src/routes/books/+layout.ts
+++ skeleton-app/src/routes/books/+layout.ts
@@ -3,7 +3,7 @@ import type { SlotTabSetting } from "$lib/types/tabSetting";
 export async function load(): Promise<{ slotTabSettings: SlotTabSetting[] }> {
   const slotTabSettings: SlotTabSetting[] = [
     { index: 0, label: "(Index)", title: "Bookリスト", path: "" },
-    { index: 1, label: "(Book 1)", title: "チートシート", path: "cheat" },
+    { index: 1, label: "(Book 1)", title: "チートシート", path: "cheat-sheet" },
     { index: 2, label: "(Book 2)", title: "プログラムの基礎", path: "pgm-basis" },
   ];
 
