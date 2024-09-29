f588fdaa4ca1c7ad66ac633d086e5b012b018244
shohei.okugi <shohei.okugi@ghelia.com>
2024-09-29 18:00:16 +0900

    :recycle: put out ContentsIndex

---
+++ skeleton-app/src/lib/components/ContentsIndex.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+  import { goto } from "$app/navigation";
+
+  export let title: string;
+  export let icon: string;
+  export let tabSettings: { label: string; title: string; path: string }[];
+  export let currentPath: string;
+</script>
+
+<div class="cBookContents">
+  <h2 class="cLessonTitle">{title}</h2>
+  <hr class="cBorderLine" />
+  <ul class="space-y-4 text-lg">
+    {#each tabSettings as { label, title, path }}
+      <li class="hover:underline">
+        <button on:click|preventDefault={() => goto(`${currentPath}/${path}`)} class="flex items-center">
+          <Icon {icon} class="mx-2" />
+          <span class="font-bold"> {label}: {title} </span>
+        </button>
+      </li>
+    {/each}
+  </ul>
+</div>
--- skeleton-app/src/routes/books/cheat-sheet/(index)/+page.svelte
+++ skeleton-app/src/routes/books/cheat-sheet/(index)/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-  import Icon from "@iconify/svelte";
   import { page } from "$app/stores";
-  import { goto } from "$app/navigation";
   import type { SlotTabSetting } from "$lib/types/tabSetting";
+  import ContentsIndex from "$lib/components/ContentsIndex.svelte";
 
   export let data: {
     slotTabSettings: SlotTabSetting[];
@@ -13,17 +12,4 @@
   $: currentPath = $page.url.pathname;
 </script>
 
-<div class="cBookContents">
-  <h2 class="cLessonTitle">チートシート</h2>
-  <hr class="cBorderLine" />
-  <ul class="space-y-4 text-lg">
-    {#each tabSettings as { label, title, path }}
-      <li class="hover:underline">
-        <button on:click|preventDefault={() => goto(`${currentPath}/${path}`)} class="flex items-center">
-          <Icon icon="mdi:script-text-outline" class="mx-2" />
-          <span class="font-bold"> {label}: {title} </span>
-        </button>
-      </li>
-    {/each}
-  </ul>
-</div>
+<ContentsIndex title="チートシート" icon="mdi:script-text-outline" {tabSettings} {currentPath} />
