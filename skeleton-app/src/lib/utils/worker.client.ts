31f7673bf5f39f954483eb9565273372cdce78ad
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-19 23:59:18 +0900

    :+1: class WebWorkerClient

--- skeleton-app/src/lib/utils/worker.client.ts
+++ skeleton-app/src/lib/utils/worker.client.ts
@@ -12,32 +12,60 @@ self.onmessage = function(event) {
 };
 `;
 
-function runScriptOnWorker<T>(
-  code: string,
-  timeout: number = 5000,
-  callback: (result: T | null, error: string | null) => void,
-): void {
-  if (!browser) {
-    callback(null, "Web Worker is not supported in this environment.");
-    return;
+class WebWorkerClient {
+  private workerScript: string;
+  private webWorker: Worker | null = null;
+  private timeoutMS: number = 5000;
+  private timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  constructor() {
+    this.workerScript = workerScript;
   }
-  const workerBlob = new Blob([workerScript], { type: "application/javascript" });
-  const worker = new Worker(URL.createObjectURL(workerBlob));
 
-  const timeoutId: ReturnType<typeof setTimeout> = setTimeout(() => {
-    worker.terminate();
-    callback(null, "Worker execution timed out.");
-  }, timeout);
-
-  worker.onmessage = function (event) {
-    clearTimeout(timeoutId);
-    if (event.data.error) {
-      callback(null, event.data.error);
-    } else {
-      callback(event.data.result, null);
+  public init(timeoutMS?: number | undefined): void {
+    if (!browser) {
+      console.warn("Web Worker is not supported in this environment.");
+      return;
     }
-  };
-  worker.postMessage(code);
+    this.timeoutMS = timeoutMS ? timeoutMS : this.timeoutMS;
+    const workerBlob = new Blob([this.workerScript], { type: "application/javascript" });
+    this.webWorker = new Worker(URL.createObjectURL(workerBlob));
+  }
+
+  private get worker(): Worker {
+    if (this.webWorker === null) {
+      throw new Error("Web Worker is not initialized.");
+    }
+    return this.webWorker;
+  }
+
+  public run<T>(code: string, callback: (result: T | null, error: string | null) => void): void {
+    this.timeoutId = setTimeout(() => {
+      this.worker.terminate();
+      console.warn("Web Worker timed out.");
+    }, this.timeoutMS);
+
+    this.worker.onmessage = (event) => {
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+        this.timeoutId = null;
+      }
+      if (event.data.error) {
+        callback(null, event.data.error);
+      } else {
+        callback(event.data.result, null);
+      }
+    };
+
+    this.worker.postMessage(code);
+  }
+
+  public terminate() {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+    this.worker.terminate();
+  }
 }
 
-export default runScriptOnWorker;
+export default WebWorkerClient;
