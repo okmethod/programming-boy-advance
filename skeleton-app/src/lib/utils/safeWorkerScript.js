69da85f0f175ceeda02ca4040d5af56d8823248c
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-21 22:49:34 +0900

    :+1: add globals from commonModules

--- skeleton-app/src/lib/utils/safeWorkerScript.js
+++ skeleton-app/src/lib/utils/safeWorkerScript.js
@@ -4,14 +4,32 @@ self.onmessage = async function (event) {
     throw new Error("allowedGlobals must be a serialized string.");
   }
 
-  let globals;
-  try {
-    globals = JSON.parse(allowedGlobals, (_, value) => {
-      if (typeof value === "string" && value.startsWith("function")) {
-        return eval(`(${value})`);
+  const workerScope = self;
+  const commonModules = Object.getOwnPropertyNames(workerScope).reduce((acc, key) => {
+    if (key in workerScope) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore: ts(7015)
+      const value = workerScope[key];
+      const type = typeof value;
+      if (type === "function" || type === "object") {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore: ts(7053)
+        acc[key] = value;
       }
-      return value;
-    });
+    }
+    return acc;
+  }, {});
+
+  let globals = {};
+  try {
+    const parsedGlobals = JSON.parse(allowedGlobals);
+    for (const [key] of Object.entries(parsedGlobals)) {
+      if (key in commonModules) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore: ts(7053)
+        globals[key] = commonModules[key];
+      }
+    }
   } catch {
     throw new Error("Failed to parse allowedGlobals.");
   }
@@ -25,6 +43,8 @@ self.onmessage = async function (event) {
     },
     get: (target, key) => {
       if (typeof key === "string" && key in target) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore: ts(7053)
         return target[key];
       }
       return undefined;
