97e3a19d85b9fd6f09860efc2c0850bb4a7c01c6
shohei.okugi <shohei.okugi@ghelia.com>
2024-10-29 23:01:37 +0900

    :+1: rename to easy labyrinth

--- skeleton-app/src/routes/labyrinth/+layout.ts
+++ skeleton-app/src/routes/labyrinth/+layout.ts
@@ -3,7 +3,7 @@ import type { SlotTabSetting } from "$lib/types/tabSetting";
 export async function load(): Promise<{ slotTabSettings: SlotTabSetting[] }> {
   const slotTabSettings: SlotTabSetting[] = [
     { index: 0, label: "(Index)", title: "迷路リスト", path: "" },
-    { index: 1, label: "(room 1)", title: "入口", path: "room1" },
+    { index: 3, label: "(room 3)", title: "迷路", path: "easy" },
   ];
 
   return { slotTabSettings };
